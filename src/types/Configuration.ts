61320937f3e6f678a61d6f6c2b0fba9682b55e73
1yizhuo1 <1yizhuo1@proton.me>
2022-09-13 16:45:42 -0500

    add config schema generator

---
+++ src/types/Configuration.ts
@@ -0,0 +1,85 @@
+interface SocialLink {
+  /**
+   * Show this social link?
+   */
+  show: boolean;
+  /**
+   * The URL of the social link
+   */
+  url: string;
+}
+
+interface Configuration {
+  /**
+   * Features to enable on this instance
+   */
+  features?: {
+    /**
+     * Additional features that are not categorized
+     */
+    additional?: {
+      ukraine?: {
+        /**
+         * Feature enabled?
+         */
+        enabled?: boolean;
+        /**
+         * Support text
+         */
+        supportText?: string;
+        /**
+         * Support link navigated on interaction
+         */
+        supportUrl?: string;
+      };
+    };
+  };
+
+  /**
+   * Footer configuration (displayed at the bottom of most pages)
+   */
+  footer?: {
+    /**
+     * Categories and their respective links
+     */
+    links?: Array<{
+      /**
+       * Category name
+       */
+      sectionName?: string;
+      /**
+       * Links in this category
+       */
+      links?: Array<{
+        /**
+         * Link name
+         */
+        label?: string;
+        /**
+         * Link navigated on interaction
+         */
+        url?: string;
+      }>;
+    }>;
+
+    /**
+     * Social links
+     */
+    socials?: {
+      /**
+       * Twitter link
+       */
+      twitter?: SocialLink;
+      /**
+       * YouTube link
+       */
+      youtube?: SocialLink;
+      /**
+       * Instagram link
+       */
+      instagram?: SocialLink;
+    };
+  };
+}
+
+export default Configuration;
--- src/util/useConfig.ts
+++ src/util/useConfig.ts
@@ -1,38 +1,6 @@
 // @ts-ignore
 import data from "../../framework.yml";
-
-interface SocialLink {
-  show: boolean;
-  url: string;
-}
-
-interface Configuration {
-  features?: {
-    additional?: {
-      ukraine?: {
-        enabled?: boolean;
-        supportText?: string;
-        supportUrl?: string;
-      };
-    };
-  };
-
-  footer?: {
-    links?: Array<{
-      sectionName?: string;
-      links?: Array<{
-        label?: string;
-        url?: string;
-      }>;
-    }>;
-
-    socials?: {
-      twitter?: SocialLink;
-      youtube?: SocialLink;
-      instagram?: SocialLink;
-    };
-  };
-}
+import Configuration from "../types/Configuration";
 
 export default function useConfig(): Configuration {
   if (!data) {
