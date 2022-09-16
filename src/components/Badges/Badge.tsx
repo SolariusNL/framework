8d0dc6d563cb0592ed083d94c66ad28a273c7425
1yizhuo1 <1yizhuo1@proton.me>
2022-09-16 18:34:20 +0000

    update badge design

--- src/components/Badges/Badge.tsx
+++ src/components/Badges/Badge.tsx
@@ -10,17 +10,25 @@ interface BadgeProps {
 
 const Badge = ({ user, title, description, icon }: BadgeProps) => (
   <Paper withBorder shadow="md" p={12} radius="md">
-    <Stack spacing={12}>
-      {icon}
-      <Stack spacing={3}>
-        <Text weight={500} size="lg">
-          {title}
-        </Text>
-        <Text size="sm" color="dimmed">
-          {description}
-        </Text>
-      </Stack>
-    </Stack>
+    <div
+      style={{
+        display: "flex",
+        gap: "16px",
+        width: "100%",
+      }}
+    >
+      <div>{icon}</div>
+      <Group>
+        <Stack spacing={3}>
+          <Text weight={500} size="lg">
+            {title}
+          </Text>
+          <Text size="sm" color="dimmed">
+            {description}
+          </Text>
+        </Stack>
+      </Group>
+    </div>
   </Paper>
 );
 
