a896f1dc2290d6cedb1adda159501da90e952d9d
1yizhuo1 <1yizhuo1@proton.me>
2022-09-30 18:34:56 +0000

    deprecate empty state

--- src/components/EmptyState.tsx
+++ src/components/EmptyState.tsx
@@ -2,28 +2,29 @@ import { Center, Group, Text } from "@mantine/core";
 import { HiExclamationCircle } from "react-icons/hi";
 
 interface EmptyStateProps {
-  title: string,
-  body: string,
-  action?: any
+  title: string;
+  body: string;
+  action?: any;
 }
 
-const EmptyState = ({
-  title,
-  body,
-  action
-}: EmptyStateProps) => {
+/**
+ * @deprecated
+ * Use ModernEmptyState instead
+ */
+const EmptyState = ({ title, body, action }: EmptyStateProps) => {
   return (
     <Center p={32}>
       <Group>
-        <HiExclamationCircle style={{
-          marginBottom: "auto"
-        }} size={32} />
+        <HiExclamationCircle
+          style={{
+            marginBottom: "auto",
+          }}
+          size={32}
+        />
         <Text size="lg" weight={550}>
           {title}
           <br />
-          <Text weight={400}>
-            {body}
-          </Text>
+          <Text weight={400}>{body}</Text>
           {action && (
             <>
               <br />
