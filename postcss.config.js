6036c48c4626696139d1322c2d09a67420ee595c
1yizhuo1 <1yizhuo1@proton.me>
2022-10-24 17:24:20 -0500

    fix css minification problem some css wouldnt end up in the compiled server

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,20 @@
 module.exports = {
   plugins: [
-    "tailwindcss",
-    "postcss-preset-env",
-    process.env.NODE_ENV === "production" ?
-      [ "@fullhuman/postcss-purgecss", {
-        content: [
-          "./src/pages/**/*.{js,jsx,ts,tsx}",
-          "./src/components/**/*.{js,jsx,ts,tsx}",
-          "./src/layouts/**/*.{js,jsx,ts,tsx}",
-        ],
-        defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-      },
-      ] : undefined,
+    // "tailwindcss",
+    // "postcss-preset-env",
+    // process.env.NODE_ENV === "production"
+    //   ? [
+    //       // "@fullhuman/postcss-purgecss",
+    //       {
+    //         content: [
+    //           // "./src/pages/**/*.{js,jsx,ts,tsx}",
+    //           // "./src/components/**/*.{js,jsx,ts,tsx}",
+    //           // "./src/layouts/**/*.{js,jsx,ts,tsx}",
+    //         ],
+    //         defaultExtractor: (content) =>
+    //           content.match(/[\w-/:]+(?<!:)/g) || [],
+    //       },
+    //     ]
+    //   : undefined,
   ],
 };
