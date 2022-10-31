3289b580a674862eb5cefdf279e223b43ea8048c
1yizhuo1 <1yizhuo1@proton.me>
2022-10-31 17:44:00 -0500

    snippet cleanup

--- src/components/ModernEmptyState.tsx
+++ src/components/ModernEmptyState.tsx
@@ -1,15 +1,32 @@
-import { Center, Stack, Text, ThemeIcon, Title } from "@mantine/core";
+import { Stack, Text, ThemeIcon, Title } from "@mantine/core";
 import { HiXCircle } from "react-icons/hi";
 
 interface ModernEmptyStateProps {
   icon?: React.ReactNode;
   title: string;
   body: string;
+  shaded?: boolean;
 }
 
-const ModernEmptyState = ({ icon, title, body }: ModernEmptyStateProps) => {
+const ModernEmptyState = ({
+  icon,
+  title,
+  body,
+  shaded,
+}: ModernEmptyStateProps) => {
   return (
-    <Stack p={32}>
+    <Stack
+      p={32}
+      sx={(theme) => ({
+        ...(shaded && {
+          backgroundColor:
+            theme.colorScheme === "dark"
+              ? theme.colors.dark[9]
+              : theme.colors.gray[1],
+          borderRadius: theme.radius.md,
+        }),
+      })}
+    >
       <div style={{ textAlign: "center" }}>
         <ThemeIcon
           sx={{
