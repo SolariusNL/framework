9d44e5d81ebe8f4dd6f68dc93f42c51c40f79e43
1yizhuo1 <1yizhuo1@proton.me>
2022-12-24 00:35:51 -0600

    post redesign

--- src/pages/blog/p/[slug].tsx
+++ src/pages/blog/p/[slug].tsx
@@ -1,4 +1,4 @@
-import { Avatar, Divider, Text, TypographyStylesProvider } from "@mantine/core";
+import { Avatar, Text, TypographyStylesProvider } from "@mantine/core";
 import { GetServerSidePropsContext, NextPage } from "next";
 import { HiClock, HiPencil, HiStar, HiUsers } from "react-icons/hi";
 import Framework from "../../../components/Framework";
@@ -24,55 +24,64 @@ const BlogPost: NextPage<BlogPostProps> = ({ post, user }) => {
         href: "/blog",
       }}
     >
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-6 md:gap-2 gap-y-8">
-        {[
-          {
-            icon: HiClock,
-            label: "Created",
-            value: <Text>{new Date(post.createdAt).toLocaleDateString()}</Text>,
-          },
-          {
-            icon: HiUsers,
-            label: "Views",
-            value: <Text>{post.views}</Text>,
-          },
-          {
-            icon: HiPencil,
-            label: "Author",
-            value: (
-              <div className="flex items-center gap-2">
-                <Avatar
-                  src={getMediaUrl(post.author.avatarUri)}
-                  size={24}
-                  radius="xl"
-                />
-                <Text inline size="sm">
-                  {post.author.username}
-                </Text>
+      <div className="flex flex-col md:flex-row">
+        <div className="flex flex-col w-full md:w-1/6">
+          <div 
+          // stack on desktop, row on mobile
+            className="flex flex-row md:flex-col justify-between gap-4 mb-6"
+          >
+            {[
+              {
+                icon: HiClock,
+                label: "Created",
+                value: (
+                  <Text size="sm">{new Date(post.createdAt).toLocaleDateString()}</Text>
+                ),
+              },
+              {
+                icon: HiUsers,
+                label: "Views",
+                value: <Text size="sm">{post.views}</Text>,
+              },
+              {
+                icon: HiPencil,
+                label: "Author",
+                value: (
+                  <div className="flex items-center gap-2">
+                    <Avatar
+                      src={getMediaUrl(post.author.avatarUri)}
+                      size={24}
+                      radius="xl"
+                    />
+                    <Text inline size="sm">
+                      {post.author.username}
+                    </Text>
+                  </div>
+                ),
+              },
+              {
+                icon: HiStar,
+                label: "Featured",
+                value: <Text size="sm">{post.featured ? "Yes" : "No"}</Text>,
+              },
+            ].map((stat, i) => (
+              <div key={i}>
+                <div>
+                  <Text color="dimmed" weight={500}>
+                    {stat.label}
+                  </Text>
+                  {stat.value}
+                </div>
               </div>
-            ),
-          },
-          {
-            icon: HiStar,
-            label: "Featured",
-            value: <Text>{post.featured ? "Yes" : "No"}</Text>,
-          },
-        ].map((stat, i) => (
-          <div key={i} className="text-center w-full">
-            <stat.icon size={24} />
-            <div className="flex items-center gap-2 text-center justify-center mt-1">
-              <Text inline color="dimmed" weight={500}>
-                {stat.label}
-              </Text>
-              {stat.value}
-            </div>
+            ))}
           </div>
-        ))}
+        </div>
+        <div className="flex flex-col w-full md:w-5/6">
+          <TypographyStylesProvider>
+            <div dangerouslySetInnerHTML={{ __html: post.content }} />
+          </TypographyStylesProvider>
+        </div>
       </div>
-      <Divider mt={32} mb={32} />
-      <TypographyStylesProvider>
-        <div dangerouslySetInnerHTML={{ __html: post.content }} />
-      </TypographyStylesProvider>
     </Framework>
   );
 };
