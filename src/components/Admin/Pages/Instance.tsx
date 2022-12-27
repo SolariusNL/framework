7f1023d9efe3b13608fd6d6829652fcd89071f10
1yizhuo1 <1yizhuo1@proton.me>
2022-12-27 17:09:51 -0600

    instance changes

--- src/components/Admin/Pages/Instance.tsx
+++ src/components/Admin/Pages/Instance.tsx
@@ -1,7 +1,7 @@
-import { Alert, Skeleton, Title } from "@mantine/core";
+import { Anchor, Divider, Skeleton, Text, Title } from "@mantine/core";
 import { getCookie } from "cookies-next";
 import { useEffect, useState } from "react";
-import { HiExclamationCircle } from "react-icons/hi";
+import { HiEmojiHappy, HiEmojiSad, HiExternalLink } from "react-icons/hi";
 
 const Instance = () => {
   const [version, setVersion] = useState("");
@@ -30,32 +30,70 @@ const Instance = () => {
   return (
     <>
       <Title mb={24} order={2}>
-        Framework{" "}
-        {!version ? (
-          <span
-            style={{
-              display: "inline-flex",
-            }}
-          >
-            <Skeleton height={32} width={74} />
-          </span>
-        ) : (
-          version
-        )}
+        Framework
       </Title>
 
-      {version !== latestVersion && (
-        <Alert
-          mb={24}
-          color="red"
-          icon={<HiExclamationCircle size="24" />}
-          title="Update available"
-        >
-          You are running Framework {version}, but the latest version is{" "}
-          {latestVersion}. Please update your instance to get the latest
-          features and bug fixes.
-        </Alert>
-      )}
+      <div className="grid grid-cols-3 gap-4">
+        <div className="col-span-3 md:col-span-1">
+          <Text color="dimmed" size="sm" mb={8}>
+            Installed
+          </Text>
+          <Anchor
+            href={`https://github.com/Tsodinq/framework/releases/tag/${version}`}
+            target="_blank"
+            className="flex items-center"
+          >
+            <Title order={2} mr={8}>
+              {!version ? <Skeleton height={32} width={74} /> : version}
+            </Title>
+            <HiExternalLink />
+          </Anchor>
+        </div>
+        <div className="col-span-3 md:col-span-1">
+          <Text color="dimmed" size="sm" mb={8}>
+            Latest
+          </Text>
+          <Anchor
+            href={`https://github.com/Tsodinq/framework/releases/tag/${version}`}
+            target="_blank"
+            className="flex items-center"
+          >
+            <Title order={2} mr={8}>
+              {!latestVersion ? (
+                <Skeleton height={32} width={74} />
+              ) : (
+                latestVersion
+              )}
+            </Title>
+            <HiExternalLink />
+          </Anchor>
+        </div>
+        <div className="col-span-3 md:col-span-1">
+          {version && latestVersion && version !== latestVersion ? (
+            <div className="flex items-center gap-2 flex-wrap">
+              <HiEmojiSad size={32} className="text-red-500" />
+              <Text color="red">
+                You are not running the latest version of Framework. Please
+                update to the latest version to ensure you are receiving the
+                latest security updates.
+              </Text>
+            </div>
+          ) : (
+            <div className="flex items-center gap-2 flex-wrap">
+              <HiEmojiHappy size={32} className="text-green-500" />
+              <Text color="green">
+                You are running the latest version of Framework.
+              </Text>
+            </div>
+          )}
+        </div>
+      </div>
+
+      <Divider mt={32} mb={32} />
+
+      <Title mb={24} order={2}>
+        Database
+      </Title>
     </>
   );
 };
