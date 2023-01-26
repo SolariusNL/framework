a9a08c5150933ed1a296bfbb53ba7774fb9001cd
1yizhuo1 <1yizhuo1@proton.me>
2023-01-26 17:36:18 -0600

    add ticket create email

---
+++ email/emails/support-ticket-created.tsx
@@ -0,0 +1,59 @@
+import { Head } from "@react-email/head";
+import { Hr } from "@react-email/hr";
+import { Html } from "@react-email/html";
+import { Preview } from "@react-email/preview";
+import { Text } from "@react-email/text";
+import { styles } from "./framework";
+import EmailLayout from "./layout";
+
+interface SupportTicketCreatedProps {
+  title: string;
+  content: string;
+  category: string;
+  contactEmail: string;
+  contactName: string;
+}
+
+const SupportTicketCreated: React.FC<SupportTicketCreatedProps> = ({
+  title = "My Ticket",
+  content = "I need help with my account. I can't seem to log in, and I'm not sure why. I've tried resetting my password, but it still doesn't work.",
+  category = "Account",
+  contactEmail = "example@test.com",
+  contactName = "John Doe",
+}) => {
+  return (
+    <Html>
+      <Head />
+      <Preview>Support Ticket Created</Preview>
+      <EmailLayout
+        title={<>Support ticket created</>}
+        footer={
+          <>
+            This is an automated email to remind you have created a support
+            ticket. If this wasn&apos;t you, you can safely ignore this email.
+          </>
+        }
+      >
+        <Text style={styles.text}>
+          Your support ticket, <b>{title}</b> has been created and received by
+          our support team. We will get back to you as soon as possible.
+        </Text>
+        <Hr />
+        <Text style={styles.text}>
+          <b>Category:</b> {category}
+        </Text>
+        <Text style={styles.text}>
+          <b>Content:</b> {content}
+        </Text>
+        <Text style={styles.text}>
+          <b>Contact Email:</b> {contactEmail}
+        </Text>
+        <Text style={styles.text}>
+          <b>Contact Name:</b> {contactName}
+        </Text>
+      </EmailLayout>
+    </Html>
+  );
+};
+
+export default SupportTicketCreated;
--- src/pages/api/support/[[...params]].ts
+++ src/pages/api/support/[[...params]].ts
@@ -13,6 +13,8 @@ import prisma from "../../../util/prisma";
 import { SupportTicketStatus } from "@prisma/client";
 import { sendMail } from "../../../util/mail";
 import getMediaUrl from "../../../util/getMedia";
+import { render } from "@react-email/render";
+import SupportTicketCreated from "../../../../email/emails/support-ticket-created";
 
 class SupportRouter {
   @Post("/submit")
@@ -72,53 +74,17 @@ class SupportRouter {
     sendMail(
       String(process.env.SUPPORT_EMAIL),
       "New support ticket",
-      `
-      <div style="max-width: 600px; margin: 0 auto; padding: 20px; box-sizing: border-box;">
-        <h1 style="font-size: 24px; color: #333; margin-bottom: 20px;">New Support Ticket</h1>
-        <p style="font-size: 16px; color: #666;">A new support ticket has been submitted on the website:</p>
-        <ul style="list-style: none; margin: 0; padding: 0;">
-            <li style="margin-bottom: 10px;">
-              <strong style="display: block; font-size: 16px; color: #333;">Title:</strong> ${
-                data.data.title
-              }
-            </li>
-            <li style="margin-bottom: 10px;">
-              <strong style="display: block; font-size: 16px; color: #333;">Content:</strong> ${
-                data.data.content
-              }
-            </li>
-            <li style="margin-bottom: 10px;">
-              <strong style="display: block; font-size: 16px; color: #333;">Category:</strong> ${
-                data.data.category.substring(0, 1).toUpperCase() +
-                data.data.category.substring(1)
-              }
-            </li>
-            <li style="margin-bottom: 10px;">
-              <strong style="display: block; font-size: 16px; color: #333;">Contact Email:</strong> ${
-                data.data.contactEmail
-              }
-            </li>
-            <li style="margin-bottom: 10px;">
-              <strong style="display: block; font-size: 16px; color: #333;">Contact Name:</strong> ${
-                data.data.contactName
-              }
-            </li>
-        </ul>
-        <h2 style="font-size: 20px; color: #333; margin-top: 20px; margin-bottom: 10px;">User</h2>
-        <div style="display: flex; align-items: center;">
-            ${
-              user
-                ? `<img src="${getMediaUrl(
-                    user.avatarUri
-                  )}" alt="User Avatar" style="width: 24px; height: 24px; margin-right: 10px;" />
-              <strong style="font-size: 16px; color: #333;">${
-                user.username
-              }</strong>`
-                : "<strong style=\"font-size: 16px; color: #333;\">Anonymous</strong>"
-            }
-        </div>
-      </div>
-      `
+      render(
+        SupportTicketCreated({
+          title: data.data.title,
+          content: data.data.content,
+          category:
+            data.data.category.substring(0, 1).toUpperCase() +
+            data.data.category.substring(1),
+          contactEmail: data.data.contactEmail,
+          contactName: data.data.contactName || "N/A",
+        }) as React.ReactElement
+      )
     );
     sendMail(
       data.data.contactEmail,
