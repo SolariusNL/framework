5ff6ebfb7ac9c5140b78429d4e496564171c382d
1yizhuo1 <1yizhuo1@proton.me>
2023-01-30 18:40:28 +0000

    add loaders

--- src/components/Home/FeedWidget.tsx
+++ src/components/Home/FeedWidget.tsx
@@ -3,6 +3,7 @@ import {
   Avatar,
   Button,
   Divider,
+  Loader,
   Menu,
   Modal,
   Skeleton,
@@ -39,7 +40,7 @@ type StatusPost = StatusPosts & {
 };
 
 const FeedWidget: React.FC = () => {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const user = useFrameworkUser()!;
   const [statusPosts, setStatusPosts] = useState<StatusPost[]>([]);
   const [reportOpened, setReportOpened] = useState(false);
@@ -59,7 +60,6 @@ const FeedWidget: React.FC = () => {
   });
 
   const handleStatusPost = async (values: { status: string }) => {
-    setLoading(true);
     await fetch("/api/users/@me/status", {
       method: "POST",
       body: JSON.stringify({ status: values.status }),
@@ -76,10 +76,10 @@ const FeedWidget: React.FC = () => {
           setNewPost(false);
         }
       });
-    setLoading(false);
   };
 
   const getStatusPosts = async () => {
+    setLoading(true);
     await fetch("/api/users/@me/statusposts", {
       headers: {
         Authorization: String(getCookie(".frameworksession")),
@@ -90,7 +90,8 @@ const FeedWidget: React.FC = () => {
         if (res.success) {
           setStatusPosts(res.statusPosts);
         }
-      });
+      })
+      .finally(() => setLoading(false));
   };
 
   useEffect(() => {
@@ -137,83 +138,89 @@ const FeedWidget: React.FC = () => {
           </Button>
         }
       />
-      <ReactNoSSR onSSR={<Skeleton height={430} />}>
-        {statusPosts !== undefined && statusPosts.length === 0 ? (
-          <ShadedCard>
-            <ModernEmptyState
-              title="No status posts yet"
-              body="Your friends will post here when they have something to say."
-            />
-          </ShadedCard>
-        ) : (
-          <>
-            <Stack spacing={"md"}>
-              {statusPosts.map((status) => (
-                <ShadedCard key={status.id}>
-                  <div>
-                    <div className="flex justify-between">
-                      <div className="flex">
-                        <div>
-                          <UserContext user={status.user}>
-                            <Avatar
-                              size={42}
-                              src={getMediaUrl(status.user.avatarUri)}
-                              mr={12}
-                              className="rounded-full"
-                            />
-                          </UserContext>
-                        </div>
-                        <div className="flex-grow">
+      {loading ? (
+        <ShadedCard className="flex items-center justify-center py-8">
+          <Loader />
+        </ShadedCard>
+      ) : (
+        <>
+          {statusPosts && statusPosts.length === 0 ? (
+            <ShadedCard>
+              <ModernEmptyState
+                title="No status posts yet"
+                body="Your friends will post here when they have something to say."
+              />
+            </ShadedCard>
+          ) : (
+            <>
+              <Stack spacing={"md"}>
+                {statusPosts.map((status) => (
+                  <ShadedCard key={status.id}>
+                    <div>
+                      <div className="flex justify-between">
+                        <div className="flex">
                           <div>
-                            <Text mb={2} size="sm">
-                              @{status.user.username}
-                            </Text>
-                            <Text color="dimmed" size="xs">
-                              {getRelativeTime(
-                                new Date(status.createdAt as Date)
-                              )}
-                            </Text>
+                            <UserContext user={status.user}>
+                              <Avatar
+                                size={42}
+                                src={getMediaUrl(status.user.avatarUri)}
+                                mr={12}
+                                className="rounded-full"
+                              />
+                            </UserContext>
                           </div>
-                          <Text mt={12}>{status.content}</Text>
+                          <div className="flex-grow">
+                            <div>
+                              <Text mb={2} size="sm">
+                                @{status.user.username}
+                              </Text>
+                              <Text color="dimmed" size="xs">
+                                {getRelativeTime(
+                                  new Date(status.createdAt as Date)
+                                )}
+                              </Text>
+                            </div>
+                            <Text mt={12}>{status.content}</Text>
+                          </div>
                         </div>
-                      </div>
-                      <div>
-                        <Menu width={180}>
-                          <Menu.Target>
-                            <ActionIcon>
-                              <HiDotsVertical />
-                            </ActionIcon>
-                          </Menu.Target>
-                          <Menu.Dropdown>
-                            <Menu.Item
-                              color="red"
-                              icon={<HiFlag />}
-                              onClick={() => {
-                                setReportUser(status.user);
-                                setReportOpened(true);
-                              }}
-                            >
-                              Report
-                            </Menu.Item>
-                            <Menu.Item
-                              icon={<HiClipboardCopy />}
-                              onClick={() => {
-                                navigator.clipboard.writeText(status.content);
-                              }}
-                            >
-                              Copy content
-                            </Menu.Item>
-                          </Menu.Dropdown>
-                        </Menu>
+                        <div>
+                          <Menu width={180}>
+                            <Menu.Target>
+                              <ActionIcon>
+                                <HiDotsVertical />
+                              </ActionIcon>
+                            </Menu.Target>
+                            <Menu.Dropdown>
+                              <Menu.Item
+                                color="red"
+                                icon={<HiFlag />}
+                                onClick={() => {
+                                  setReportUser(status.user);
+                                  setReportOpened(true);
+                                }}
+                              >
+                                Report
+                              </Menu.Item>
+                              <Menu.Item
+                                icon={<HiClipboardCopy />}
+                                onClick={() => {
+                                  navigator.clipboard.writeText(status.content);
+                                }}
+                              >
+                                Copy content
+                              </Menu.Item>
+                            </Menu.Dropdown>
+                          </Menu>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </ShadedCard>
-              ))}
-            </Stack>
-          </>
-        )}
-      </ReactNoSSR>
+                  </ShadedCard>
+                ))}
+              </Stack>
+            </>
+          )}
+        </>
+      )}
     </>
   );
 };
