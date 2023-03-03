fc3d2dbb37ea92bebf0129a4233047abaee6e5f7
1yizhuo1 <1yizhuo1@proton.me>
2023-03-03 18:44:30 +0000

    blog button fix

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -193,7 +193,7 @@ const Blog: NextPage<BlogProps> = ({ user, posts, featured }) => {
                     <div className="px-2 grow flex">
                       <UnstyledButton
                         type="submit"
-                        className="bg-sky-500 flex-auto shadow text-white text-sm border-y border-transparent py-2 font-semibold px-0 hover:bg-sky-600 dark:hover:bg-sky-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-300 dark:focus:ring-offset-slate-900 dark:focus:ring-sky-700 highlight-white/25 disabled:opacity-50 disabled:cursor-not-allowed text-center"
+                        className="bg-sky-500 flex-auto h-fit shadow text-white text-sm border-y border-transparent py-2 font-semibold px-2 hover:bg-sky-600 dark:hover:bg-sky-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-300 dark:focus:ring-offset-slate-900 dark:focus:ring-sky-700 highlight-white/25 disabled:opacity-50 disabled:cursor-not-allowed text-center"
                         style={{ borderRadius: "8px" }}
                         disabled={loading}
                       >
