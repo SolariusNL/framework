44ca1656a43d15854dd4a1f14a57f611a017464f
1yizhuo1 <1yizhuo1@proton.me>
2023-03-07 16:00:19 -0600

    star animation

--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -1,4 +1,5 @@
 import { Tooltip, UnstyledButton } from "@mantine/core";
+import { motion } from "framer-motion";
 import { useState } from "react";
 import { HiStar } from "react-icons/hi";
 import clsx from "../util/clsx";
@@ -26,28 +27,40 @@ const Rating: React.FC<RatingProps> = (props) => {
             label={`Rate experience ${index} star${index > 1 ? "s" : ""}`}
             key={index}
           >
-            <UnstyledButton
-              type="button"
-              className={clsx(
-                index <= (hover || props.value || 0)
-                  ? "dark:text-yellow-300 text-yellow-500"
-                  : "text-gray-400/50",
-                "text-3xl",
-                props.loading &&
-                  index <= props.value! &&
-                  "dark:text-yellow-300/30 text-yellow-500/30 cursor-not-allowed"
-              )}
-              onClick={() => {
-                if (props.loading) return;
-                props.setValue(index);
-                if (props.onRatingConfirmation)
-                  props.onRatingConfirmation(index);
+            <motion.div
+              animate={{
+                scale: hover === index ? 1.19 : 1,
               }}
-              onMouseEnter={() => setHover(index)}
-              onMouseLeave={() => setHover(props.value || 0)}
+              transition={{
+                type: "spring",
+                stiffness: 260,
+                damping: 20,
+              }}
             >
-              <HiStar />
-            </UnstyledButton>
+              <UnstyledButton
+                type="button"
+                className={clsx(
+                  index <= (hover || props.value || 0)
+                    ? "dark:text-yellow-300 text-yellow-500"
+                    : "text-gray-400/50",
+                  "text-3xl",
+                  props.loading &&
+                    index <= props.value! &&
+                    "dark:text-yellow-300/30 text-yellow-500/30 cursor-not-allowed",
+                  "transition-colors duration-200 ease-in-out"
+                )}
+                onClick={() => {
+                  if (props.loading) return;
+                  props.setValue(index);
+                  if (props.onRatingConfirmation)
+                    props.onRatingConfirmation(index);
+                }}
+                onMouseEnter={() => setHover(index)}
+                onMouseLeave={() => setHover(props.value || 0)}
+              >
+                <HiStar />
+              </UnstyledButton>
+            </motion.div>
           </Tooltip>
         );
       })}
