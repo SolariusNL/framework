f075eedadb8b8799efc7015e2377ae7d0588d812
1yizhuo1 <1yizhuo1@proton.me>
2023-03-10 18:32:44 +0000

    new ban ux

--- src/pages/punished.tsx
+++ src/pages/punished.tsx
@@ -1,14 +1,18 @@
 import {
-  Container,
-  Grid,
-  SimpleGrid,
-  Skeleton,
+  Anchor,
+  Badge,
+  Button,
+  Text,
+  Title,
   useMantineTheme,
 } from "@mantine/core";
+import { getCookie } from "cookies-next";
 import { GetServerSidePropsContext } from "next";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import Framework from "../components/Framework";
+import logout from "../util/api/logout";
 import authorizedRoute from "../util/auth";
 import { User } from "../util/prisma-types";
 
@@ -29,26 +33,70 @@ const Punished: React.FC<{
 
   return (
     <Framework user={user} activeTab="none">
-      <Container my="md">
-        <SimpleGrid
-          cols={2}
-          spacing="md"
-          breakpoints={[{ maxWidth: "sm", cols: 1 }]}
+      <div className="px-4 sm:px-6 lg:px-8 mx-auto max-w-[37.5rem]">
+        <Title order={3} mb="md">
+          Account suspended
+        </Title>
+        <Text color="dimmed" size="sm" mb="xs">
+          Your account has been suspended from Framework for violating our{" "}
+          <Link href="/terms" passHref>
+            <Anchor>Terms of Service</Anchor>
+          </Link>{" "}
+          and / or the{" "}
+          <Link href="/guidelines" passHref>
+            <Anchor>Community Guidelines</Anchor>
+          </Link>
+          .
+        </Text>
+        <Text color="dimmed" size="sm" mb="lg">
+          Please review your ticket below. You may reactivate your account on
+          the date posted below, unless you have received a permanent
+          suspension.
+        </Text>
+        <Text color="dimmed" size="sm" weight={700} mb="xs">
+          Ban notice
+        </Text>
+        <Text mb="md">{user.banReason}</Text>
+        <Text color="dimmed" size="sm" weight={700} mb="xs">
+          Ban expires
+        </Text>
+        <Text mb="xl" className="flex items-center gap-2">
+          {new Date(user.banExpires as Date).toLocaleDateString("en-US", {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          })}
+          {new Date(user.banExpires as Date).getFullYear() === 9999 && (
+            <Badge radius="md" color="red">
+              Permanent Ban
+            </Badge>
+          )}
+        </Text>
+        <Button
+          fullWidth
+          variant="subtle"
+          onClick={async () => {
+            if (
+              new Date(user.banExpires as Date).getTime() <=
+              new Date().getTime()
+            ) {
+              await fetch("/api/users/@me/unlock", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                  Authorization: String(getCookie(".frameworksession")),
+                },
+              }).then(() => router.reload());
+            } else {
+              await logout().then(() => router.reload());
+            }
+          }}
         >
-          <Skeleton height={PRIMARY_COL_HEIGHT} radius="md" />
-          <Grid gutter="md">
-            <Grid.Col>
-              <Skeleton height={SECONDARY_COL_HEIGHT} radius="md" />
-            </Grid.Col>
-            <Grid.Col span={6}>
-              <Skeleton height={SECONDARY_COL_HEIGHT} radius="md" />
-            </Grid.Col>
-            <Grid.Col span={6}>
-              <Skeleton height={SECONDARY_COL_HEIGHT} radius="md" />
-            </Grid.Col>
-          </Grid>
-        </SimpleGrid>
-      </Container>
+          {new Date(user.banExpires as Date).getTime() <= new Date().getTime()
+            ? "Unlock my account"
+            : "Logout of your account"}
+        </Button>
+      </div>
     </Framework>
   );
 };
