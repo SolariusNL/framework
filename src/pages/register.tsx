01948e3b0ff8a51ad765784ecc673b0b559d68b4
1yizhuo1 <1yizhuo1@proton.me>
2023-04-03 00:04:20 -0500

    typeform waitlist

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,12 +1,18 @@
 import {
   Anchor,
   Button,
-  Checkbox, Group, Stack, TextInput
+  Checkbox,
+  Group,
+  MantineColor,
+  MantineTheme,
+  Stack,
+  TextInput,
 } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import type { GetServerSidePropsContext, NextPage } from "next";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import Script from "next/script";
 import OuterUI from "../layouts/OuterUI";
 import authorizedRoute from "../util/auth";
 import { setCookie } from "../util/cookies";
@@ -84,64 +90,119 @@ const Register: NextPage = () => {
       });
   };
 
+  const OAUTH_STYLE = (theme: MantineTheme, color: MantineColor) => ({
+    border: `1px solid ${theme.colors[color][9]}`,
+  });
+
   return (
-    <OuterUI
-      description={
-        <span>
-          Framework is in alpha and requires an invite code to join.{" "}
-          <Link href="/login">
-            <Anchor>Have an account already? Login to your account.</Anchor>
-          </Link>
-        </span>
-      }
-    >
-      <form onSubmit={form.onSubmit((values) => handleRegister(values))}>
-        <Stack spacing={12}>
-          <TextInput
-            label="Invite code"
-            description="You should've received this in your email. If you cannot locate your invite code, please contact us."
-            placeholder="0000-0000-0000-0000"
-            required
-            {...form.getInputProps("code")}
-          />
+    <>
+      <Script
+        src="//embed.typeform.com/next/embed.js"
+        strategy="afterInteractive"
+      />
+      <OuterUI
+        description={
+          <span>
+            Framework is in alpha and requires an invite code to join.{" "}
+            <Link href="/login">
+              <Anchor>Have an account already? Login to your account.</Anchor>
+            </Link>
+          </span>
+        }
+      >
+        <form onSubmit={form.onSubmit((values) => handleRegister(values))}>
+          <Stack spacing={12}>
+            <TextInput
+              label="Invite code"
+              description="You should've received this in your email. If you cannot locate your invite code, please contact us."
+              placeholder="0000-0000-0000-0000"
+              required
+              {...form.getInputProps("code")}
+            />
 
-          <TextInput
-            label="Username"
-            description="Your username is unique to you."
-            placeholder="Framework"
-            required
-            {...form.getInputProps("username")}
-          />
+            <TextInput
+              label="Username"
+              description="Your username is unique to you."
+              placeholder="Framework"
+              required
+              {...form.getInputProps("username")}
+            />
 
-          <TextInput
-            label="Email"
-            type="email"
-            description="Used to send you password reset emails, and for general protection."
-            placeholder="Enter an email"
-            required
-            {...form.getInputProps("email")}
-          />
+            <TextInput
+              label="Email"
+              type="email"
+              description="Used to send you password reset emails, and for general protection."
+              placeholder="Enter an email"
+              required
+              {...form.getInputProps("email")}
+            />
 
-          <TextInput
-            label="Password"
-            type="password"
-            description="Your password is used to login to your account."
-            placeholder="Enter a password"
-            required
-            {...form.getInputProps("password")}
-          />
-        </Stack>
-        <Group position="apart" mt="md">
-          <Checkbox
-            label="Remember me"
-            {...form.getInputProps("remainLoggedIn")}
-          />
-        </Group>
-        <Button fullWidth mt="xl" type="submit">
-          Register
-        </Button>
-      </form>
-    </OuterUI>
+            <TextInput
+              label="Password"
+              type="password"
+              description="Your password is used to login to your account."
+              placeholder="Enter a password"
+              required
+              {...form.getInputProps("password")}
+            />
+          </Stack>
+          <Group position="apart" mt="md">
+            <Checkbox
+              label="Remember me"
+              {...form.getInputProps("remainLoggedIn")}
+            />
+          </Group>
+          <Button fullWidth mt="xl" type="submit" mb="sm">
+            Register
+          </Button>
+          <Button
+            variant="subtle"
+            color="gray"
+            fullWidth
+            data-tf-popup="Diz4hyUu"
+            data-tf-opacity="100"
+            data-tf-size="100"
+            data-tf-iframe-props="title=Framework Waitlist"
+            data-tf-transitive-search-params
+            data-tf-medium="snippet"
+          >
+            Join the waitlist
+          </Button>
+          {/**
+           * @todo Add OAuth signup flow
+           */}
+          {/* <div className="flex flex-col md:flex-row items-center gap-4">
+          <ActionIcon
+            variant="light"
+            color="blue"
+            className="w-full"
+            size="xl"
+            sx={(theme) => OAUTH_STYLE(theme, "blue")}
+          >
+            <BsDiscord size={20} />
+          </ActionIcon>
+          <ActionIcon
+            variant="light"
+            className="w-full"
+            size="xl"
+            sx={(theme) => OAUTH_STYLE(theme, "orange")}
+            color="orange"
+          >
+            <BsGoogle size={20} />
+          </ActionIcon>
+          <ActionIcon
+            variant="light"
+            color="indigo"
+            className="w-full"
+            size="xl"
+            sx={(theme) => OAUTH_STYLE(theme, "indigo")}
+          >
+            <BsTwitter size={20} />
+          </ActionIcon>
+        </div> */}
+        </form>
+      </OuterUI>
+    </>
   );
 };
 
