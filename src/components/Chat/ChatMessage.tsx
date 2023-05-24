97356f0364a7d003a51d070fe7ed9516c1cd0911
1yizhuo1 <1yizhuo1@proton.me>
2023-05-24 02:07:41 -0500

    fix

--- src/components/Chat/ChatMessage.tsx
+++ src/components/Chat/ChatMessage.tsx
@@ -49,7 +49,6 @@ const ChatMessage: FC<ChatMessageProps> = ({
         onOpen={() => setMenuOpened(true)}
         onClose={() => setMenuOpened(false)}
         withinPortal
-        zIndex={99999}
       >
         <Menu.Target>
           <ActionIcon>
