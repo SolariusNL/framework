bb53689efa8ac61a2cfece3e6c00b7e9b1164c08
1yizhuo1 <1yizhuo1@proton.me>
2023-07-07 18:57:49 -0500

    remove gift code premium

--- src/pages/api/gift/[[...params]].ts
+++ src/pages/api/gift/[[...params]].ts
@@ -1,8 +1,8 @@
-import { GiftCodeGrant } from "@prisma/client";
-import { createHandler, Param, Post } from "@storyofams/next-api-decorators";
 import Authorized, { Account } from "@/util/api/authorized";
 import prisma from "@/util/prisma";
 import type { User } from "@/util/prisma-types";
+import { GiftCodeGrant } from "@prisma/client";
+import { createHandler, Param, Post } from "@storyofams/next-api-decorators";
 
 class GiftRouter {
   @Post("/redeem/:code")
@@ -57,74 +57,20 @@ class GiftRouter {
         });
         break;
       case GiftCodeGrant.PREMIUM_ONE_MONTH:
-        if (user.premium) {
-          return {
-            success: false,
-            message: "User already has premium",
-          };
-        }
-
-        await prisma.user.update({
-          where: {
-            id: Number(user.id),
-          },
-          data: {
-            premium: true,
-            premiumSubscription: {
-              create: {
-                type: "PREMIUM_ONE_MONTH",
-                expiresAt: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
-              },
-            },
-          },
-        });
-        break;
+        return {
+          success: false,
+          message: "Stripe now handles premium subscriptions.",
+        };
       case GiftCodeGrant.PREMIUM_ONE_YEAR:
-        if (user.premium) {
-          return {
-            success: false,
-            message: "User already has premium",
-          };
-        }
-
-        await prisma.user.update({
-          where: {
-            id: Number(user.id),
-          },
-          data: {
-            premium: true,
-            premiumSubscription: {
-              create: {
-                type: "PREMIUM_ONE_YEAR",
-                expiresAt: new Date(Date.now() + 365 * 24 * 60 * 60 * 1000),
-              },
-            },
-          },
-        });
-        break;
+        return {
+          success: false,
+          message: "Stripe now handles premium subscriptions.",
+        };
       case GiftCodeGrant.PREMIUM_SIX_MONTHS:
-        if (user.premium) {
-          return {
-            success: false,
-            message: "User already has premium",
-          };
-        }
-
-        await prisma.user.update({
-          where: {
-            id: Number(user.id),
-          },
-          data: {
-            premium: true,
-            premiumSubscription: {
-              create: {
-                type: "PREMIUM_SIX_MONTHS",
-                expiresAt: new Date(Date.now() + 180 * 24 * 60 * 60 * 1000),
-              },
-            },
-          },
-        });
-        break;
+        return {
+          success: false,
+          message: "Stripe now handles premium subscriptions.",
+        };
       case GiftCodeGrant.SIXTEENTHOUSAND_TICKETS:
         await prisma.user.update({
           where: {
