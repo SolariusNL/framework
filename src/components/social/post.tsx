db4482f30f49dd0888b7d59a39f023ee0079c56c
1yizhuo1 <1yizhuo1@proton.me>
2023-09-11 17:48:00 -0500

    finish social card design

--- src/components/social/post.tsx
+++ src/components/social/post.tsx
@@ -1,12 +1,20 @@
 import ShadedButton from "@/components/shaded-button";
+import UserContext from "@/components/user-context";
+import Verified from "@/components/verified";
 import clsx from "@/util/clsx";
+import { Fw } from "@/util/fw";
 import getMediaUrl from "@/util/get-media";
 import { NonUser } from "@/util/prisma-types";
-import { ActionIcon, Avatar } from "@mantine/core";
+import { getRelativeTime } from "@/util/relative-time";
+import { ActionIcon, Avatar, Text } from "@mantine/core";
+import dynamic from "next/dynamic";
 import Link from "next/link";
 import { FC } from "react";
 import { HiOutlineDotsVertical, HiOutlineFlag } from "react-icons/hi";
-import Verified from "../verified";
+
+const RenderMarkdown = dynamic(() => import("../render-markdown"), {
+  ssr: false,
+});
 
 type SocialPostProps = {
   post: SocialPost;
@@ -23,15 +31,36 @@ type SocialPost = {
 
 const SocialPost: FC<SocialPostProps> = ({ post }) => {
   return (
-    <ShadedButton>
-      <div className="flex w-full justify-between gap-4 items-start">
-        <li className={clsx("flex items-start font-medium whitespace-nowrap")}>
-          <Avatar
-            src={getMediaUrl(post.author.avatarUri)}
-            alt=""
-            className={clsx("mr-3 rounded-full")}
-            size={38}
-          />
+    <ShadedButton
+      sx={(theme) => ({
+        padding: theme.spacing.xs,
+        borderRadius: theme.radius.md,
+        width: "100%",
+        display: "flex",
+        transition: "background-color 0.05s ease",
+        "&:hover": {
+          backgroundColor:
+            theme.colorScheme === "dark"
+              ? theme.colors.dark[9] + "10"
+              : theme.colors.gray[1] + "75",
+        },
+      })}
+    >
+      <div className="flex justify-between gap-1 items-start w-full">
+        <li className={clsx("flex items-start")}>
+          <UserContext user={post.author}>
+            <Avatar
+              src={getMediaUrl(post.author.avatarUri)}
+              alt=""
+              className={clsx("mr-3 rounded-full")}
+              size={38}
+              color={Fw.Strings.color(post.author.username)}
+            >
+              {Fw.Strings.initials(post.author.username)}
+            </Avatar>
+          </UserContext>
+        </li>
+        <div className="flex flex-col gap-1 w-full font-medium">
           <div className={clsx("leading-4 flex items-center gap-2 text-base")}>
             <div className={clsx("flex items-center gap-1 flex-row-reverse")}>
               {post.author.verified && (
@@ -43,19 +72,29 @@ const SocialPost: FC<SocialPostProps> = ({ post }) => {
             </div>
             <div>
               <Link href={`/profile/${post.author.username}`} passHref>
-                <a className="text-dimmed no-underline">
+                <a className="text-dimmed no-underline text-sm">
                   @{post.author.username}
                 </a>
               </Link>
             </div>
           </div>
-        </li>
-        <div className="flex items-center gap-2">
-          <ActionIcon color="red" size="lg" radius="xl" variant="light">
-            <HiOutlineFlag />
-          </ActionIcon>
+          {typeof window !== "undefined" && (
+            <RenderMarkdown proseAddons="break-words font-normal">
+              {post.contentMd}
+            </RenderMarkdown>
+          )}
+          <div className="mt-4 flex flex-wrap gap-2">
+            <Text size="sm" color="dimmed">
+              {getRelativeTime(post.createdAt)}
+            </Text>
+          </div>
+        </div>
+        <div className="flex flex-col flex-shrink-0 items-center gap-2">
           <ActionIcon color="gray" size="lg" radius="xl" variant="light">
             <HiOutlineDotsVertical />
+          </ActionIcon>
+          <ActionIcon color="red" size="lg" radius="xl" variant="light">
+            <HiOutlineFlag />
           </ActionIcon>
         </div>
       </div>
--- src/pages/social/index.tsx
+++ src/pages/social/index.tsx
@@ -1,9 +1,13 @@
 import Framework from "@/components/framework";
-import SocialPost from "@/components/social/post";
 import authorizedRoute from "@/util/auth";
 import { NonUser, User } from "@/util/prisma-types";
 import { GetServerSideProps } from "next";
+import dynamic from "next/dynamic";
 import { FC } from "react";
+
+const SocialPost = dynamic(() => import("@/components/social/post"), {
+  ssr: false,
+});
 
 type SocialProps = {
   user: User;
@@ -13,13 +17,21 @@ const posts = [
   {
     author: {
       id: 3,
-      username: "thebaristabarnyc",
-      alias: "BaristaBar",
+      username: "james",
+      alias: "James",
       avatarUri: "https://github.com/torvalds.png",
       verified: true,
+      _count: {
+        followers: 32,
+        following: 12,
+      },
     } as NonUser,
-    createdAt: new Date(),
-    contentMd: "**hello**, world! *test*",
+    createdAt: new Date(Date.now() - 128 * 60 * 60 * 1000),
+    contentMd: `
+'Those who play with fire shall perish by it' - [Xi Jinping](https://en.wikipedia.org/wiki/Xi_Jinping)
+'玩火者必自焚' - [Xi Jinping](https://en.wikipedia.org/wiki/Xi_Jinping)
+
+American Goons, Your Days Are Numbered.`,
     _count: {
       hearts: 13,
       shares: 5,
