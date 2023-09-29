6d2d8c740804e99f7a1e4b06c69af5ed0d536556
1yizhuo1 <1yizhuo1@proton.me>
2023-09-29 13:14:02 -0500

    updates

--- src/util/auth.ts
+++ src/util/auth.ts
@@ -1,74 +1,10 @@
-import cast from "@/util/cast";
 import { exclude } from "@/util/exclude";
-import { getIPAddressGeolocation } from "@/util/geo";
 import prisma from "@/util/prisma";
 import { User, userSelect } from "@/util/prisma-types";
+import { OAuthApplication, Session } from "@prisma/client";
 import { GetServerSidePropsContext } from "next";
 import { getClientIp } from "request-ip";
-
-export const checkSession = async (
-  token: string
-): Promise<User | undefined> => {
-  const session = await prisma.session.findFirst({
-    where: { token },
-    include: { oauth: true },
-  });
-
-  if (!session || !session.userId || session.oauth) {
-    return undefined;
-  }
-
-  const account = await prisma.user.findFirst({
-    where: { id: session.userId },
-    select: {
-      ...exclude(userSelect, "games"),
-      password: true,
-      recentIp: true,
-    },
-  });
-
-  return cast<User>(JSON.parse(JSON.stringify(account)));
-};
-
-const updateLastSeenAndBits = async (account: User | undefined) => {
-  if (account) {
-    await prisma.user.update({
-      where: { id: account.id },
-      data: { lastSeen: new Date() },
-    });
-
-    if (
-      !account.lastDailyBits ||
-      new Date(account.lastDailyBits).getTime() <
-        Date.now() - 24 * 60 * 60 * 1000
-    ) {
-      await prisma.user.update({
-        where: { id: account.id },
-        data: {
-          lastDailyBits: new Date(),
-          bits: {
-            increment: 100,
-          },
-        },
-      });
-    }
-  }
-};
-
-const isIpBanned = async (context: GetServerSidePropsContext) => {
-  const bannedIps = await prisma.bannedIP.findMany({
-    where: { ip: String(getClientIp(context.req)) },
-  });
-
-  return bannedIps.length > 0;
-};
-
-const redirectTo = (destination: string) => ({
-  redirect: {
-    destination,
-    permanent: false,
-  },
-});
+import { getIPAddressGeolocation } from "./geo";
 
 const authorizedRoute = async (
   context: GetServerSidePropsContext,
@@ -76,27 +12,40 @@ const authorizedRoute = async (
   redirectIfAuthorized: boolean = false,
   redirectIfNotAdmin: boolean = false,
   ...props: any[]
-): Promise<{
-  props?: {
-    user?: User;
-  };
-  redirect?: {
-    destination: string;
-    permanent: boolean;
-  };
-}> => {
+) => {
   const token = context.req.cookies[".frameworksession"];
 
-  if (!token) {
-    return redirectIfNotAuthorized
-      ? redirectTo("/login")
-      : { props: { user: cast<User>(undefined) } };
+  if (token === undefined) {
+    if (redirectIfNotAuthorized) {
+      return {
+        redirect: {
+          destination: "/login",
+          permanent: false,
+        },
+      };
+    } else {
+      return {
+        props: { ...props },
+      };
+    }
   }
 
-  const account = await checkSession(token);
+  let account = await getAccountFromSession(token);
+  const isAuthorized = !!account;
 
-  if (await isIpBanned(context)) {
-    return redirectTo("/403");
+  const bannedIps = await prisma.bannedIP.findMany({
+    where: {
+      ip: String(getClientIp(context.req)),
+    },
+  });
+
+  if (bannedIps.length > 0) {
+    return {
+      redirect: {
+        destination: "/403",
+        permanent: false,
+      },
+    };
   }
 
   const ip = getClientIp(context.req);
@@ -105,31 +54,163 @@ const authorizedRoute = async (
     getIPAddressGeolocation(ip!).then(async (geo) => {
       if (!geo.error) {
         await prisma.user.update({
-          where: { id: account?.id },
-          data: { recentIp: geo.ip, recentIpGeo: geo },
+          where: {
+            id: account?.id,
+          },
+          data: {
+            recentIp: geo.ip,
+            recentIpGeo: geo,
+          },
         });
       }
     });
   }
 
-  if (account) {
-    await updateLastSeenAndBits(account);
+  switch (isAuthorized) {
+    case true:
+      await prisma.user.update({
+        where: {
+          id: account?.id,
+        },
+        data: {
+          lastSeen: new Date(),
+        },
+      });
 
-    if (account.banned && context.resolvedUrl !== "/punished")
-      return redirectTo("/punished");
-    if (account.locked && context.resolvedUrl !== "/locked")
-      return redirectTo("/locked");
-    if (redirectIfAuthorized) return redirectTo("/");
-    if (redirectIfNotAdmin && account.role !== "ADMIN") return redirectTo("/");
+      if (account?.banned && context.resolvedUrl !== "/punished") {
+        return {
+          redirect: {
+            destination: "/punished",
+            permanent: false,
+            banRedirect: true,
+          },
+        };
+      }
+      if (account?.locked && context.resolvedUrl !== "/locked") {
+        return {
+          redirect: {
+            destination: "/locked",
+            permanent: false,
+            banRedirect: true,
+          },
+        };
+      }
 
-    return {
-      props: {
-        user: { ...account },
-      },
-    };
-  } else {
-    return redirectIfNotAuthorized ? redirectTo("/login") : { props: {} };
+      if (
+        account?.lastDailyBits === null ||
+        new Date(account?.lastDailyBits!).getTime() <
+          Date.now() - 24 * 60 * 60 * 1000
+      ) {
+        await prisma.user.update({
+          where: {
+            id: account?.id,
+          },
+          data: {
+            lastDailyBits: new Date(),
+            bits: {
+              increment: 100,
+            },
+          },
+        });
+        account = {
+          ...account,
+          bits: account?.bits! + 100,
+        } as typeof account;
+      }
+
+      if (redirectIfAuthorized) {
+        return {
+          redirect: {
+            destination: "/",
+            permanent: false,
+          },
+        };
+      } else {
+        if (redirectIfNotAdmin && account!.role !== "ADMIN") {
+          return {
+            redirect: {
+              destination: "/",
+              permanent: false,
+            },
+          };
+        }
+
+        return {
+          props: {
+            user: {
+              ...account,
+            },
+          },
+        };
+      }
+    case false:
+      if (redirectIfNotAuthorized) {
+        return {
+          redirect: {
+            destination: "/login",
+            permanent: false,
+          },
+        };
+      } else {
+        return { props: {} };
+      }
   }
 };
+
+async function verifySession(
+  token: string
+): Promise<(Session & { oauth: OAuthApplication }) | undefined> {
+  const session = JSON.parse(
+    JSON.stringify(
+      await prisma.session.findFirst({
+        where: {
+          token,
+        },
+        include: { oauth: true },
+      })
+    )
+  );
+
+  if (!session) {
+    return undefined;
+  }
+
+  return session;
+}
+
+export async function getAccountFromSession(
+  token: string
+): Promise<User | undefined> {
+  const session = await verifySession(token);
+
+  if (!session || !session.userId) {
+    return undefined;
+  }
+
+  if (session.oauth) {
+    return undefined;
+  }
+
+  const account = JSON.parse(
+    JSON.stringify(
+      await prisma.user.findFirst({
+        where: {
+          id: session.userId,
+        },
+        select: {
+          ...exclude(userSelect, "games"),
+          password: true,
+          recentIp: true,
+        },
+      })
+    )
+  );
+
+  if (!account) {
+    return undefined;
+  }
+
+  return account;
+}
 
 export default authorizedRoute;
