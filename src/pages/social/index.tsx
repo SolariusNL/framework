57eb123b4b02ed098ecf820bc5882b7a81c00a43
Tsodinq <1yizhuo1@proton.me>
2024-01-04 13:43:21 +0000

    add layout

--- src/pages/social/index.tsx
+++ src/pages/social/index.tsx
@@ -1,14 +1,15 @@
 import Framework from "@/components/framework";
+import SidebarTabNavigation from "@/layouts/SidebarTabNavigation";
+import {
+  GetSocialPostsResponse,
+  SocialPost,
+} from "@/pages/api/social/[[...params]]";
 import authorizedRoute from "@/util/auth";
 import fetchJson from "@/util/fetch";
 import { User } from "@/util/prisma-types";
 import { GetServerSideProps } from "next";
 import dynamic from "next/dynamic";
 import { FC, useEffect, useState } from "react";
-import {
-  GetSocialPostsResponse,
-  SocialPost,
-} from "../api/social/[[...params]]";
 
 const SocialPost = dynamic(() => import("@/components/social/post"), {
   ssr: false,
@@ -34,14 +35,21 @@ const Social: FC<SocialProps> = ({ user }) => {
 
   return (
     <Framework user={user} activeTab="social" noPadding>
-      <div className="w-full flex justify-center mt-12">
-        <div className="max-w-xl w-full px-4">
-          <div className="flex flex-col gap-2">
-            {posts?.length > 0 &&
-              posts.map((p, i) => <SocialPost post={p} key={i} />)}
+      <SidebarTabNavigation>
+        <SidebarTabNavigation.Sidebar>
+          <code>Todo add filters</code>
+        </SidebarTabNavigation.Sidebar>
+        <SidebarTabNavigation.Content>
+          <div className="w-full flex justify-center mt-12">
+            <div className="max-w-xl w-full px-4">
+              <div className="flex flex-col gap-2">
+                {posts?.length > 0 &&
+                  posts.map((p, i) => <SocialPost post={p} key={i} />)}
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        </SidebarTabNavigation.Content>
+      </SidebarTabNavigation>
     </Framework>
   );
 };
